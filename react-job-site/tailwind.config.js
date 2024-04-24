9001af508722b7242f34a8770adf332fb2a32fa4
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-24 20:39:46 +0800

    added font config

--- react-job-site/tailwind.config.js
+++ react-job-site/tailwind.config.js
@@ -2,7 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: { sans: ["Roboto", "sans-serif"] },
+    },
   },
   plugins: [],
 };
