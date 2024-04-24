348b0ba03f472b718a4bf462190c1c1068ee1864
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-24 23:46:29 +0800

    imported separated components

--- react-job-site/src/App.tsx
+++ react-job-site/src/App.tsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+      {/* <!-- Hero --> */}
+      <Hero />
+
+      {/* <!-- Developers and Employers --> */}
+      <HomeCards />
+      {/* <!-- Browse Jobs --> */}
+      <JobListings />
+
+      <section className="max-w-lg px-6 m-auto my-10">
+        <a
+          href="jobs.html"
+          className="block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
 }
 
 export default App;
