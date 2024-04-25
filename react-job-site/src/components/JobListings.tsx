2afffa7260e0692a2267af5dc77313c859a73aed
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-25 23:15:47 +0800

    separated the joblisting and pass the job prop

--- react-job-site/src/components/JobListings.tsx
+++ react-job-site/src/components/JobListings.tsx
@@ -1,4 +1,5 @@
 import jobs from "../jobs.json";
+import JobListing from "./JobListing";
 
 const JobListings = () => {
   console.log(jobs);
@@ -10,33 +11,7 @@ const JobListings = () => {
         </h2>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           {jobs.map((job) => (
-            <div className="relative bg-white shadow-md rounded-xl">
-              <div className="p-4">
-                <div className="mb-6">
-                  <div className="my-2 text-gray-600">{job.type}</div>
-                  <h3 className="text-xl font-bold">{job.title}</h3>
-                </div>
-
-                <div className="mb-5">{job.description}</div>
-
-                <h3 className="mb-2 text-indigo-500">{job.salary}</h3>
-
-                <div className="mb-5 border border-gray-100"></div>
-
-                <div className="flex flex-col justify-between mb-4 lg:flex-row sm:flex-column">
-                  <div className="mb-3 text-orange-700">
-                    <i className="text-lg fa-solid fa-location-dot"></i>
-                    {job.location}
-                  </div>
-                  <a
-                    href={`job/${job.id}`}
-                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                  >
-                    Read More
-                  </a>
-                </div>
-              </div>
-            </div>
+            <JobListing job={job} />
           ))}
         </div>
       </div>
