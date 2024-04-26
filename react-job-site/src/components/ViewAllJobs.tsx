b74190890c949547113be7721693810b8789b877
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-26 15:32:53 +0800

    created separate component

---
+++ react-job-site/src/components/ViewAllJobs.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ViewAllJobs = () => {
+  return (
+    <section className="max-w-lg px-6 m-auto my-10">
+      <a
+        href="jobs.html"
+        className="block px-6 py-4 text-center text-white bg-black rounded-xl hover:bg-gray-700"
+      >
+        View All Jobs
+      </a>
+    </section>
+  );
+};
+
+export default ViewAllJobs;
