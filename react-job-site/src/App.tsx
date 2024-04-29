31305c3e205e17185343cc58a2a6ccb448ae5f99
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-29 23:20:30 +0800

    routed the job page

--- react-job-site/src/App.tsx
+++ react-job-site/src/App.tsx
@@ -8,11 +8,13 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
+import JobPage from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
+      <Route path="/job" element={<JobPage />} />
     </Route>
   )
 );
