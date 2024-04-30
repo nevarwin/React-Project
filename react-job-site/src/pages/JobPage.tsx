7c24e1dffbeaaeb51288918002d940940c44a094
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-30 23:50:00 +0800

    used the JobListings component with its prop

--- react-job-site/src/pages/JobPage.tsx
+++ react-job-site/src/pages/JobPage.tsx
@@ -1,6 +1,11 @@
+import JobListings from "../components/JobListings";
 
 const JobPage = () => {
-  return <div>YEET</div>;
+  return (
+    <section className="px-4 py-6 bg-blue-50">
+      <JobListings isHome={false} />
+    </section>
+  );
 };
 
 export default JobPage;
