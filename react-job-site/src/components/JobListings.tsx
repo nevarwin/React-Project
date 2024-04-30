f80ae6abfef411c47345f89e16704b475de6087d
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-04-30 23:49:01 +0800

    added a isHome parameter, modify the title and data passed in JobListing component based on paramter

--- react-job-site/src/components/JobListings.tsx
+++ react-job-site/src/components/JobListings.tsx
@@ -1,17 +1,22 @@
 import jobs from "../jobs.json";
 import JobListing from "./JobListing";
 
-const JobListings = () => {
-  console.log(jobs);
+interface JobListingsProp {
+  isHome: boolean;
+}
+
+const JobListings: React.FC<JobListingsProp> = ({ isHome }) => {
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+
   return (
     <section className="px-4 py-10 bg-blue-50">
       <div className="m-auto container-xl lg:container">
         <h2 className="mb-6 text-3xl font-bold text-center text-indigo-500">
-          Browse Jobs
+          {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {jobs.map((job) => (
-            <JobListing job={job} />
+          {jobListings.map((job) => (
+            <JobListing key={job.id} job={job} />
           ))}
         </div>
       </div>
