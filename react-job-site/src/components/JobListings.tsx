2aa908dec354a727e288234881c314862c212b34
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-01 23:27:30 +0800

    make the data fetching from the json file

--- react-job-site/src/components/JobListings.tsx
+++ react-job-site/src/components/JobListings.tsx
@@ -1,12 +1,33 @@
-import jobs from "../jobs.json";
+import { useState, useEffect } from "react";
+
 import JobListing from "./JobListing";
+import { job } from "../model/job";
 
 interface JobListingsProp {
   isHome: boolean;
 }
 
 const JobListings: React.FC<JobListingsProp> = ({ isHome }) => {
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  // const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+
+  const [jobs, setJobs] = useState<job[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  // getting the data from the jobs.json file
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/jobs");
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        alert(`Failed to fetch jobs ${error}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchJobs();
+  }, []);
 
   return (
     <section className="px-4 py-10 bg-blue-50">
@@ -15,9 +36,15 @@ const JobListings: React.FC<JobListingsProp> = ({ isHome }) => {
           {isHome ? "Recent Jobs" : "Browse Jobs"}
         </h2>
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-          ))}
+          {loading ? (
+            <h2>Loading...</h2>
+          ) : (
+            <>
+              {jobs.map((job) => (
+                <JobListing key={job.id} job={job} />
+              ))}
+            </>
+          )}
         </div>
       </div>
     </section>
