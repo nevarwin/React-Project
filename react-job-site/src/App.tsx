d77af27d8b04a7d0b5f0412c7b21f80595e49a3a
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-05 00:15:09 +0800

    added the single job page in the router

--- react-job-site/src/App.tsx
+++ react-job-site/src/App.tsx
@@ -10,12 +10,14 @@ import HomePage from "./pages/HomePage";
 import MainLayout from "./layouts/MainLayout";
 import JobPage from "./pages/JobPage";
 import NotFound from "./pages/NotFound";
+import SingleJobPage from "./pages/SingleJobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobPage />} />
+      <Route path="/jobs/:id" element={<SingleJobPage />} />
       <Route path="*" element={<NotFound />} />
     </Route>
   )
