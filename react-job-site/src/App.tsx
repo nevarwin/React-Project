05e53468aacf1680d3493e84dab42f92de1fa1f8
Raven Solis <86557872+nevarwin@users.noreply.github.com>
2024-05-08 23:50:32 +0800

    added function for adding a job then pass the function as a prop

--- react-job-site/src/App.tsx
+++ react-job-site/src/App.tsx
@@ -12,22 +12,33 @@ import JobPage from "./pages/JobPage";
 import NotFound from "./pages/NotFound";
 import SingleJobPage from "./pages/SingleJobPage";
 import AddJobPage from "./pages/AddJobPage";
+import { job } from "./model/job";
 // import SingleJobPage, { jobLoader } from "./pages/SingleJobPage";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<MainLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/jobs" element={<JobPage />} />
-      <Route path="/jobs/:id" element={<SingleJobPage />} />
-      <Route path="/add-job" element={<AddJobPage />} />
-      {/* <Route path="/jobs/:id" element={<SingleJobPage />} loader={jobLoader} /> */}
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  )
-);
-
 function App() {
+  const addJob = async (newJob: job) => {
+    await fetch("api/jobs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/jobs" element={<JobPage />} />
+        <Route path="/jobs/:id" element={<SingleJobPage />} />
+        <Route path="/add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+        {/* <Route path="/jobs/:id" element={<SingleJobPage />} loader={jobLoader} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    )
+  );
   return <RouterProvider router={router} />;
 }
 
